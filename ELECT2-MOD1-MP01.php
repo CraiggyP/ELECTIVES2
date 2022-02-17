bc6b6614dd034404fc1b9075cbf13b388befa6f8
CraiggyP <99894097+CraiggyP@users.noreply.github.com>
2022-02-17 23:23:24 +0800

    Add files via upload

---
+++ ELECT2-MOD1-MP01.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+          ELECT2-MOD1-MP01 / PHP Code Blocks 
+</title>
+</head>
+<body>
+    <?php 
+         $name="Craig Braudrick S. Pangilinan";
+         $birthday="March 30, 2000";
+         $gender="Male";
+         $address="Telabastagan, City Of San Fernando Pampanga";
+         $fname="Enrico Pangilinan";
+         $mname="Rowena Pangilinan";
+         $sib1="Francheska Gizelle Pangilinan";
+         $sib2="Franzelle Gelyn Pangilinan";
+    ?> 
+<?php
+    echo "<b>PERSONAL INFORMATION</b> <br>";
+    echo "Name: ","$name<br>";
+    echo "Birthday: ","$birthday<br>";
+    echo "Gender: ","$gender<br>";
+    echo "Home Address: ", "$address<br>";
+	echo"<br>";
+
+    echo "<b>FAMILY BACKGROUND</b> <br>";
+    echo "Father's Name: ","$fname<br>";
+    echo "Mother's Name: ","$mname<br>";
+    echo "Sibling 1: ","$sib1<br>";
+    echo "Sibling 2: ","$sib2<br>";
+	echo"<br>";
+
+    echo "<b>EDUCATIONAL BACKGROUND</b> <br>";
+	echo"<br>";
+    echo "Primary School:Achievers Special Education Center<br>";
+    echo "Date Graduated:2013 <br>";
+    echo"<br>";
+    echo "Junior Highschool: Mother of Good Counsel Seminary/RCC Colegio De San Rafael <br>";
+    echo "Date Graduated: 2017 <br>";
+    echo"<br>";
+    echo "Senior High School: RCC Colegio De San Rafael <br>";
+    echo "Date Graduated: 2019 <br>";
+    echo"<br>";
+    echo "Tertiary School: Angeles University Foundation <br>";
+    echo "Course: BS Computer Engineering <br>";
+    echo"<br>";
+    echo "<b>FAVORITE FOODS</b> <br>";
+    echo "1. Kare-Kare <br>";
+    echo "2. Tacos <br>";
+    echo "3. Pizza <br>";
+    echo "4. Carbonara  <br>";
+    echo "5. Chicken Sandwich  <br>";
+	echo"<br>";
+    echo "<b>FAVORITE MOVIE</b> <br>";
+    echo "I dont have a specific one but I like movie genres that are: action, anime, comedy, fiction, romance.  <br>";
+    echo"<br>";
+    echo "<b>FAVORITE RAPPERS</b> <br>";
+    echo "(in no particular order)<br>";
+    echo "1. J.Cole   <br>";
+    echo "2. Lil Uzi Vert  <br>";
+    echo "3. Lil Baby  <br>";
+    echo "4. Gunna <br>";
+    echo "5. Drake <br>";
+?>
+</body>
+<html>
